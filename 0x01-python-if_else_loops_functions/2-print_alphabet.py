964d04c575740b8a486d8f781897cf33ecf66f7e
Samie <samrasolomon40@gmail.com>
2021-11-16 15:38:52 +0000

    Modified files

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for char in range(97, 123):
-    print(chr(char), end='')
+for ch in range(97, 123):
+    print("{:c}".format(ch), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for char in range(97, 123):
-    if char not in[101, 113]:
-        print(chr(char), end="")
+for ch in range(97, 123):
+    if ((ch != 101) and (ch != 113)):
+        print("{:c}".format(ch), end="")
