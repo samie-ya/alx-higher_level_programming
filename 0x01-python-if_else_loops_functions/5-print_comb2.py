b4b5f610e834791f026082a166799ba3d2e22bc0
Samie <samrasolomon40@gmail.com>
2021-11-16 14:04:53 +0000

    Added files

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for i in range(0, 100):
+	if (i < 10):
+		print("0{:d}, ".format(i), end="")
+	else:
+		if (i < 99):
+			print("{:d}, ".format(i), end="")
+		else:
+			print(i)
