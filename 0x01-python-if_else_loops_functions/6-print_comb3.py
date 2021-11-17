fdc764108e17129bca6c9f29b9357f01870825ae
Samie <samrasolomon40@gmail.com>
2021-11-17 02:17:40 +0000

    Added file

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+for i in range(0, 10):
+
+    for j in range(0, 10):
+
+        if ((i < j) and not ((i == 8) and (j == 9))):
+            print("{:d}{:d}, ".format(i, j), end="")
+
+        if ((i == 8) and (j == 9)):
+            print("{:d}{:d}".format(i, j))
