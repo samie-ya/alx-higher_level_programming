bfc015cf25cdf70b1216f75d0c19da63e009cc07
Samie <samrasolomon40@gmail.com>
2021-11-18 10:39:53 +0000

    Modified a file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,11 +2,11 @@
 
 if __name__ == "__main__":
 
-	import calculator_1 as calc
+    import calculator_1 as calc
 
-	a = 10
-	b = 5
-	print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
-	print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
-	print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
-	print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
