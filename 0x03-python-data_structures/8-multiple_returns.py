f200797152ec5ba1c7806e4ff5a089f8c33b8d18
Samie <samrasolomon40@gmail.com>
2021-11-22 08:17:07 +0000

    Modified a file

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,8 @@
 
 
 def multiple_returns(sentence):
-	for i in range(len(sentence)):
-		if (len(sentence) > 0):
-			return len(sentence), sentence[0]
-		else:
-			return 
-		
+    for i in range(len(sentence)):
+        if (len(sentence) == 0):
+            return len(sentence), None
+        else:
+            return len(sentence), sentence[0]
