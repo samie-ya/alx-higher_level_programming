0ac925dcb97c9905dbb3a1f283df4ec0e098dad6
Samie <samrasolomon40@gmail.com>
2021-11-22 07:51:10 +0000

    Modified a file

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,7 @@
 
 def print_reversed_list_integer(my_list=[]):
 
-    if len(my_list) != 0:
+    if my_list:
         length = (len(my_list) - 1)
         while(length >= 0):
             print("{:d}".format(my_list[length]))
