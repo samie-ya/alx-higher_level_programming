3a424f77ad9172dd3d98c5902164ecdfee399ad4
Samie <samrasolomon40@gmail.com>
2021-11-22 07:56:40 +0000

    Modified a file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,7 +6,7 @@ def new_in_list(my_list, idx, element):
     lists = my_list[:]
     if idx < 0:
         return lists
-    elif idx > len(lists):
+    elif idx >= len(lists):
         return lists
     else:
         for i in range(len(lists)):
