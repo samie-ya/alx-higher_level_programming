b8b8b8817afb1dcf683d84c9becab8be3387ab0d
Samie <samrasolomon40@gmail.com>
2021-11-24 18:11:04 +0000

    Added a file

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    for i in list(a_dictionary):
+        if (value in a_dictionary[i]):
+            del a_dictionary[i]
+    return a_dictionary
