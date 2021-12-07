6c0a3e6eba876418ef74b74d573507e89204ba36
Samie <samrasolomon40@gmail.com>
2021-12-07 16:34:21 +0000

    Added a file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defining Square in class"""
+
+
+class Square:
+    """This is class is to compute area of a square"""
+    def __init__(self, size = 0):
+        """Class is initialized with a field named size.
+
+        Args:
+            size (float): size is a new field it has been initialized to 0.
+        """
+        self.__size = size
