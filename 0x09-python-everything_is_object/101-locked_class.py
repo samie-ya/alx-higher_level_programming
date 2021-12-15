933a03e77a88fd5c4524d393dd0c552e5e9b6eaa
Samie <samrasolomon40@gmail.com>
2021-12-15 21:28:38 +0000

    Modified a filw

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""Defining a locked class"""
 
 
 class LockedClass:
+    """This class is used to create a dynamic creation
+       of attributes. only those mentioned in list can be
+       created"""
+    __slots__ = ['first_name']
+
+    def __init__(self, name=None):
+        """This function is used to initialize the class
+
+           Args:
+                name (str): the value of first name"""
+        self.first_name = name
