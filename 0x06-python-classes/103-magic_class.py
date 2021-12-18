c528bf51222f608aa53063fb6f9e700f3ea022a5
Samie <samrasolomon40@gmail.com>
2021-12-18 21:02:16 +0000

    Added a file

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defining MagicClass"""
+import math
+class MagicClass:
+    """MagicClass is used to show circle circuma nd area"""
+    def __init__(self, radius=0):
+        """This function is used to initilize MagicClass
+           Args:
+                radius (int): radius of a circle"""
+        self.__radius = radius
+        if (type(radius) is not int) or (type(radius) is not float):
+            raise TypeError("radius must be a number")
+
+    def area(self):
+        """This funcnction is used to find area
+           of a circle"""
+        return (math.pi * self.__radius**2)
+
+    def circumference(self):
+        """This function is used to find circumference"""
+        return ((2 * math.pi) * self.__radius)
