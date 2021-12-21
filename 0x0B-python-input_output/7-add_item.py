fefbb5af4615b2647288a9c4816a51046b0dd719
Samie <samrasolomon40@gmail.com>
2021-12-21 21:54:07 +0000

    Modified a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""This function is used to add arguments to JSON file"""
 
 if __name__ == "__main__":
 
@@ -8,12 +9,11 @@ json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
     file_name = "add_item.json"
+    if (len(sys.argv) == 1):
+        save_to_json_file([], file_name)
     new_list = load_from_json_file(file_name)
-    i = 0
+    i = 1
     while (i < len(sys.argv)):
-        if (i == 0):
-            save_to_json_file(new_list, file_name)
-        else:
-            new_list.append(sys.argv[i])
-            save_to_json_file(new_list, file_name)
+        new_list.append(sys.argv[i])
+        save_to_json_file(new_list, file_name)
         i += 1
