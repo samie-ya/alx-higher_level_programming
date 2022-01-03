e65b1ade93906091835c504265ac1c5be31ee583
Samie <samrasolomon40@gmail.com>
2022-01-03 20:45:44 +0000

    Modified a file

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -51,3 +51,39 @@ class TestBase(unittest.TestCase):
         dic = b.to_dictionary()
         json_dict = Base.to_json_string(dic)
         self.assertEqual(json_dict, '{"id": 5, "width": 10, "height": 7, "x": 2, "y": 8}')
+
+    def test_save_to_file(self):
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), '[]')
+
+    def test_save_to_file_rec(self):
+        b = Rectangle(10, 7, 2, 8, 5)
+        Rectangle.save_to_file([b])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), '[{"id": 5, "width": 10, "height": 7, "x": 2, "y": 8}]')
+
+    def test_save_to_file_rec_2(self):
+        b = Rectangle(10, 7, 2, 8, 5)
+        b1 = Rectangle(8, 5, 4, 5, 6)
+        Rectangle.save_to_file([b, b1])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), '[{"id": 5, "width": 10, "height": 7, "x": 2, "y": 8}, {"id": 6, "width": 8, "height": 5, "x": 4, "y": 5}]')
+
+    def test_save_to_file_square(self):
+        Square.save_to_file(None)
+        with open("Square.json", "r") as f:
+            self.assertEqual(f.read(), '[]')
+
+    def test_save_to_file_square_2(self):
+        s = Square(10, 7, 2, 8)
+        Square.save_to_file([s])
+        with open("Square.json", "r") as f:
+            self.assertEqual(f.read(), '[{"id": 8, "size": 10, "x": 7, "y": 2}]')
+
+    def test_save_to_file_square_3(self):
+        s = Square(10, 7, 2, 8)
+        s1 = Square(2, 3, 4, 5)
+        Square.save_to_file([s, s1])
+        with open("Square.json", "r") as f:
+            self.assertEqual(f.read(), '[{"id": 8, "size": 10, "x": 7, "y": 2}, {"id": 5, "size": 2, "x": 3, "y": 4}]')
