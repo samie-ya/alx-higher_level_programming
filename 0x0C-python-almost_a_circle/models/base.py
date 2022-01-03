cceaa98cedb564bbe3b2fe54d8bf540ccb311a64
Samie <samrasolomon40@gmail.com>
2022-01-03 09:18:43 +0000

    Modified a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defining the class Base"""
+import json
 
 
 class Base:
@@ -19,3 +20,37 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """This function takes a list of ictionaries
+           and returns the json representation
+
+           Args:
+               list_dictionaries (list): list of dictionaries
+
+           Returns:
+               json represenation
+        """
+        if list_dictionaries is None or list_dictionaries is []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """This function write the json representation of list_obj
+           to file
+
+           Args:
+               list_objs (list): the list of instances
+        """
+        new_list = []
+        for i in list_objs:
+            new_list.append(i.to_dictionary())
+            file_name = type(i).__name__ + ".json"
+        with open(file_name, "w", encoding='utf-8') as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                f.write(cls.to_json_string(new_list))
