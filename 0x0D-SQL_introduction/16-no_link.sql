3a9c76a1ade6520501e52efbfa935efb6a0a431c
Samie <samrasolomon40@gmail.com>
2022-02-01 22:25:36 +0000

    Added files

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- This script lists all records of the table second_table of the database
 -- hbtn_0c_0
-SELECT * FROM second_table WHERE name IS NOT NULL; 
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
