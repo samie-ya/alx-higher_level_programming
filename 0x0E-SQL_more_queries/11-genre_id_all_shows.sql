1fb8c002bb1f0f29ddce0a1be6b989434b6c3c1b
Samie <samrasolomon40@gmail.com>
2022-02-02 19:27:45 +0000

    Added files

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- This script lists all shows contained in hbtn_0d_tvshows that have
 -- atleast one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
-  FROM tv_show_genres
-  INNER JOIN tv_shows
+  FROM tv_shows
+  LEFT JOIN tv_show_genres
   ON tv_shows.id = tv_show_genres.show_id
   ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
