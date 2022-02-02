77fb0c1e224d415b61f2a273ca02bf648babd0b2
Samie <samrasolomon40@gmail.com>
2022-02-02 23:39:42 +0000

    Modified a file

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,5 +6,5 @@ SELECT tv_genres.name
   ON tv_genres.id = tv_show_genres.genre_id
   INNER JOIN tv_shows
   ON tv_shows.id = tv_show_genres.show_id
-  WHERE tv_shows.id = 8
+  WHERE tv_shows.title = 'Dexter'
   ORDER BY tv_genres.name ASC;
