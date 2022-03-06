32d2b054c4dbb678f9990b31b2fa307f097257b3
Samie <samrasolomon40@gmail.com>
2022-03-06 17:44:33 +0000

    Modified a file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This file prints all state from database"""
 
+import MySQLdb
+
 if __name__ == "__main__":
 
-    import MySQLdb
     db = MySQLdb.connect(host="localhost", port=3306, user="root",
                          passwd="root", db="hbtn_0e_0_usa",
                          charset="utf8")
