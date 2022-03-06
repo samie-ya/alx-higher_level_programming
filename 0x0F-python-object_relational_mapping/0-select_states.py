d1e7a72b147493dfd930bfc2d937fdd104b4204d
Samie <samrasolomon40@gmail.com>
2022-03-06 18:12:49 +0000

    Modified a file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,16 +2,13 @@
 """This file prints all state from database"""
 
 import MySQLdb
-
-if __name__ == "__main__":
-
-    db = MySQLdb.connect(host="localhost", port=3306, user="root",
-                         passwd="root", db="hbtn_0e_0_usa",
-                         charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for r in rows:
-        print(r)
-    cur.close()
-    db.close()
+db = MySQLdb.connect(host="localhost", port=3306, user="root",
+                     passwd="root", db="hbtn_0e_0_usa",
+                     charset="utf8")
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+rows = cur.fetchall()
+for r in rows:
+    print(r)
+cur.close()
+db.close()
