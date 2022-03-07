8c1dc38ddf07b504d4bd67d30b863665639bcf11
Samie <samrasolomon40@gmail.com>
2022-03-07 02:05:56 +0000

    Modified a file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This file will create a class called State"""
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
@@ -8,5 +9,6 @@ Base = declarative_base()
 class State(Base):
     """This is the class State with class attribute id and name"""
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128))
