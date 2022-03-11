6113bb2d5a6a6aa61c31089dd581811a5da83bbe
Samie <samrasolomon40@gmail.com>
2022-03-11 20:43:59 +0000

    Modified a fil

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,18 +5,13 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    try:
         db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY \
+        cur.execute("SELECT * FROM states WHERE name LIKE '[N]%' ORDER BY \
                      states.id ASC")
         rows = cur.fetchall()
         for r in rows:
             print(r)
         cur.close()
-    except MySQLdb.Error as error:
-        print("Failed to read data", error)
-    finally:
-        if db:
-            db.close()
+        db.close()
