8a77511f3db64464063a0d9ab0ab430823061cff
Samie <samrasolomon40@gmail.com>
2022-03-16 14:35:01 +0000

    Added a file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """This script prints the name of the state given as an input"""
 
+import MySQLdb
+import sys
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}' ORDER BY states.id ASC"
                 .format(sys.argv[4]))
     rows = cur.fetchall()
     for r in rows:
