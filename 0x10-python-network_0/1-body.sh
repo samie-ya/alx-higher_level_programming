a349fc3dc0ff2184592fa8961c73a3b643eaaba7
Samie <samrasolomon40@gmail.com>
2022-03-17 21:05:18 +0000

    Modified a file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This cript request to a URL passed as an argument, displays only the status
-if [ "$(curl -sI "$1" | head -n 1 | awk '{print $2}')" -eq "200" ]; then curl -s "$1"; fi
+curl -sX GET "$1"
