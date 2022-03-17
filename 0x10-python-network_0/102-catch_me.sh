7424710e739c1bd514552f23e7c4e4c7087fb5ec
Samie <samrasolomon40@gmail.com>
2022-03-17 19:02:45 +0000

    Modified a file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This script that takes in a URL, request to that URL
-curl -sX POST 0.0.0.0:5000/catch_me -H "Content-Type: text/html" -d 'You got me!'
+curl -sX PUT 0.0.0.0:5000/catch_me -H "Content-Type: text/html" -d 'You got me!'
