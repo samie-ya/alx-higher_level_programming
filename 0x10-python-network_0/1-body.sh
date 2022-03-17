ff2065ab6ebe78a6be47445ef0cf545582567f94
Samie <samrasolomon40@gmail.com>
2022-03-17 20:25:05 +0000

    Modified a file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This cript request to a URL passed as an argument, displays only the status
-if [ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ]; then curl -s "$1"; fi;
+if [ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq "200" ]; then curl -s "$1"; fi
