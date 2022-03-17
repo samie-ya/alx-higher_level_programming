606adbe60e9e315bb9ae1ef85a12cc484653bb24
Samie <samrasolomon40@gmail.com>
2022-03-17 22:33:36 +0000

    Modified a file

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This script  takes in a URL, displays all HTTP methods the server will accept
-curl -sX OPTIONS "$1" -I | grep Allow | cut -d ' ' -f 2
+curl -sI -X OPTIONS "$1" | grep Allow | cut -d ' ' -f 2
