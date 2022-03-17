27e3b071ae1b60bbc6c8ddb411cf3971d7539e9b
Samie <samrasolomon40@gmail.com>
2022-03-17 21:10:28 +0000

    Modified a file

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script that sends a JSON POST request to a URL passed
-curl -s -X POST -d "email: test@gmail.com" -d "subject: I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
