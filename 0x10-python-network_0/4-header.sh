e4ba37634287ef29c4898ff7b0625f3bc072623b
Samie <samrasolomon40@gmail.com>
2022-03-17 19:40:39 +0000

    Modified a file

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#This script that takes in a URL, request to that URL, and displays body size
-curl -I "$1" | grep Accept-Ranges: bytes | curl -s "$1"
+# This script that takes in a URL, request to that URL, using X-School-User-Id
+curl -s -H "X-School-User-Id: 98" "$1"
