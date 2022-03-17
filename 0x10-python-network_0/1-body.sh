5f964baba7f3c8be4071059cd08bb109033dbb32
Samie <samrasolomon40@gmail.com>
2022-03-17 10:13:17 +0000

    Added a file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 #This script that takes in a URL, GET request to that URL, and displays
-curl -s "$1"
+STATUS=$(curl -s -o /dev/null -w '%{http_code}' "$1")
+if [ $STATUS -eq 200 ]
+then
+    curl -s "$1"
+fi
