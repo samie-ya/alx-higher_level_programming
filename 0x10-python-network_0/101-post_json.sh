b6ef2d7e257afc430321d7b2fc55eb70c5c3d127
Samie <samrasolomon40@gmail.com>
2022-03-17 18:21:11 +0000

    Modified a file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#This script that takes in a URL, sends a POST request to the passed URL
-curl -d 'POST params: \n\temail: test@gmail.com\n\tsubject: I will always be here for PLD' -H "Content-Type: application/json" -sX POST "$1"
+# This script that sends a JSON POST request to a URL passed
+curl -d "@$2" -H "Content-Type: application/json" -sX POST "$1"
