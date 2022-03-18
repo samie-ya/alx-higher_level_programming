831172a42c365ddb793dfde9bd3809e810b636a9
Samie <samrasolomon40@gmail.com>
2022-03-18 20:00:52 +0000

    Added a file

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This script takes your GitHub credentials (username and password) id"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    resp = requests.get('https://api.github.com/repos/' + sys.argv[1] +
+                        '/' + sys.argv[2] + '/commits')
+    json_resp = resp.json()
+    new_list = json_resp[:10]
+    for i in new_list:
+        print("{}: {}".format(i["sha"], i["commit"]["author"]["name"]))
