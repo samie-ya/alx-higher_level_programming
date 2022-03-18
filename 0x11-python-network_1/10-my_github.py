a0d37be75b47edf5581d9f275119db2748097ed1
Samie <samrasolomon40@gmail.com>
2022-03-18 19:04:39 +0000

    Modified a file

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,7 +4,8 @@
 if __name__ == "__main__":
     import requests
     import sys
-    resp = requests.get('https://api.github.com/user', auth=(sys.argv[1],
-                        sys.argv[2]))
+    resp = requests.get('https://api.github.com/user',
+                        auth=requests.auth.HTTPBasicAuth(sys.argv[1],
+                                                         sys.argv[2]))
     json_resp = resp.json()
     print(json_resp['id'])
