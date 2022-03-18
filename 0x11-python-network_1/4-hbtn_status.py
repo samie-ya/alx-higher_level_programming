12aa59d3f52e9e28c0436d4a92fd548adf2d084b
Samie <samrasolomon40@gmail.com>
2022-03-18 09:26:55 +0000

    Modified a file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- utf content: {}".format(r.text))
+    print("\t- utf8 content: {}".format(r.text))
