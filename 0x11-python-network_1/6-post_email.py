68ebe6db3dcf2359c87928133e10eb9733486f66
Samie <samrasolomon40@gmail.com>
2022-03-18 14:33:42 +0000

    Modified a file

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import requests
     import sys
     url = sys.argv[1]
-    value = sys.argv[2]
-    r = requests.post(url, email=value)
+    value = {'email': sys.argv[2]}
+    r = requests.post(url, data=value)
     print(r.text)
