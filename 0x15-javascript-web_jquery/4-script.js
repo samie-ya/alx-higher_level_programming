3573878adfed0674f9e3fb9b0118753f3325b0cd
samie-ya <samrasolomon40@gmail.com>
2022-04-20 22:02:31 +0000

    Added files

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+$('#toggle_header').click(function () {
+  if ($('header').hasClass('green')) {
+    $('header').removeClass('green');
+    $('header').addClass('red');
+  } else if ($('header').hasClass('red')) {
+    $('header').removeClass('red');
+    $('header').addClass('green');
+  } else {
+    $('header').addClass('green');
+  }
+});
